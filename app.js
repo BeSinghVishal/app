7adaaf6357971ab15b9c64c2cfdd105ed4c11799
vishal <benvishal8@gmail.com>
2021-07-06 17:30:36 +0530

    4th

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const app = require("express")();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("", (req, res) => {
+app.all("/", (req, res) => {
   res.send("Hello world");
 });
 
+function keepAlive(){
 app.listen(PORT, () => {
   console.log(`App up at port ${PORT}`);
 });
+}
+module.exports=keepAlive
