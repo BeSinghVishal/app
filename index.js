abb089d07eccc974245529f05ddd78eecdb276f6
vishal <benvishal8@gmail.com>
2021-07-06 18:29:36 +0530

    5th

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
- //const fetch=require("node-fetch")
+ const fetch=require("node-fetch")
 const Discord=require("discord.js");
 const keepAlive = require("./app");
 const client = new Discord.Client();
@@ -16,24 +16,24 @@ if(str[0]==="$vs"){
   pin=str[1]
  date=str[2]
 }
-// fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}`).then(value=>{
-// return value.json()})
-// .then(ds=>{
-//   console.log(`${pin}.....${date}..`)
-//   console.log(ds.sessions);
-// ds.sessions.forEach(ele=>{
-//   const emd=new Discord.MessageEmbed()
-//   .setTitle(`${ele.name}`)
-// 	.setDescription(`${ele.address}`)
-// 	.addFields(
-// 		{ name: `${ele.min_age_limit}`, value: `${ele.vaccine}` },
-// 		{ name: 'DOSE-1', value: `${ele.available_capacity_dose1}`, inline: true },
-// 		{ name: 'DOSE-2', value: `${ele.available_capacity_dose2}`, inline: true },
-// 	)
-//   mssg.channel.send(emd)
-// })
-// }).catch(err=>console.log(err))
-//console.log(`got the meassage as${pin}...${date}`);
+fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}`).then(value=>{
+return value.json()})
+.then(ds=>{
+  console.log(`${pin}.....${date}..`)
+  console.log(ds.sessions);
+ds.sessions.forEach(ele=>{
+  const emd=new Discord.MessageEmbed()
+  .setTitle(`${ele.name}`)
+	.setDescription(`${ele.address}`)
+	.addFields(
+		{ name: `${ele.min_age_limit}`, value: `${ele.vaccine}` },
+		{ name: 'DOSE-1', value: `${ele.available_capacity_dose1}`, inline: true },
+		{ name: 'DOSE-2', value: `${ele.available_capacity_dose2}`, inline: true },
+	)
+  mssg.channel.send(emd)
+})
+}).catch(err=>console.log(err))
+console.log(`got the meassage as${pin}...${date}`);
   }
 })
 // // const token="ODU5MjcwMDI2NjA2MjgwNzQ0.YNqPkg.62VOdloyGgExVATiHMXvX84mVYE";
